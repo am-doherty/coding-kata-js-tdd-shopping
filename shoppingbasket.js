9e5e183c02d2444ad66db8cea24ff3345a0f8a13
am-doherty <me@anthonydoherty.co.uk>
2012-08-21 16:26:32 +0100

    Added second test. Scan refactored to store item type, getTotal now a function returning value of A unless item type is B.

--- shoppingbasket.js
+++ shoppingbasket.js
@@ -6,14 +6,19 @@
 	   * ItemD at .15 each
 	   *
 	   * With offers:
-	    *		Purchase 3 of ItemA for 1.30
-		*		Purchase 2 of ItemB for 0.45
+	   *	   Purchase 3 of ItemA for 1.30
+	   *	   Purchase 2 of ItemB for 0.45
 	*/
 	
 	/* ShoppingBasket constructor (not a literal)	*/
 	function ShoppingBasket() {
+		var item;
 		return {
-			scan: function(){},
-			getTotal: 0.5
+			scan: function(itemAdded){item = itemAdded},
+			getTotal: function(){
+				if(item=='B') return 0.3;
+				return 0.5;
+			}
+			
 		}
 	};
--- tests.js
+++ tests.js
@@ -1,8 +1,14 @@
 test("I scan one of Item 'A'", function() {
 		var basket = new ShoppingBasket();
 		basket.scan("A");
-		equal(basket.getTotal,0.5, "I scan a single item 'A' into an empty basket, the basket total then equals 0.5");
+		equal(basket.getTotal(),0.5, "I scan a single item 'A' into an empty basket, the basket total then equals 0.5");
 });
+test("I scan one of Item 'B'", function() {
+		var basket = new ShoppingBasket();
+		basket.scan("B");
+		equal(basket.getTotal(),0.3, "I scan a single item 'B into an empty basket, the basket total then equals 0.3");
+});
+
 
 
  
