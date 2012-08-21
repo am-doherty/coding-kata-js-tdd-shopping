12be3e3add39249caba0b2f6a787f70faeadc361
am-doherty <me@anthonydoherty.co.uk>
2012-08-21 15:45:25 +0100

    Indentation changes shoppingbasket.js

--- shoppingbasket.js
+++ shoppingbasket.js
@@ -1,5 +1,4 @@
-	/*
-	  * Shopping Kata
+	/*  Shopping Kata
 	   * Create a shopping basket system that can total up the costs of purchasing:
 	   * ItemA at .50 each
 	   * ItemB at .30 each
@@ -7,15 +6,14 @@
 	   * ItemD at .15 each
 	   *
 	   * With offers:
-	      * 	Purchase 3 of ItemA for 1.30
-		  *	Purchase 2 of ItemB for 0.45
+	    *		Purchase 3 of ItemA for 1.30
+		*		Purchase 2 of ItemB for 0.45
 	*/
-	/*
-	  * ShoppingBasket constructor (not a literal)
-	*/
+	
+	/* ShoppingBasket constructor (not a literal)	*/
 	function ShoppingBasket() {
-	return {
-		scan: function(){},
-		getTotal: 0.5
+		return {
+			scan: function(){},
+			getTotal: 0.5
 		}
 	};
